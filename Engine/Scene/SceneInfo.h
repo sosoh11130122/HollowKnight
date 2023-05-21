3a89afb3b513820ee19466ccdf1e8c9a880ad2db
sosoh11130122 <113823092+sosoh11130122@users.noreply.github.com>
2023-05-21 21:38:39 +0900

    Add files via upload

---
+++ Engine/Scene/SceneInfo.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "../EngineInfo.h"
+#include "../Render/RenderManager.h"
+
+class CSceneInfo
+{
+	friend class CScene;
+	friend class CSceneManager;
+
+protected:
+	CSceneInfo();
+	CSceneInfo(const CSceneInfo& Info);
+	virtual ~CSceneInfo();
+
+protected:
+	class CScene* m_Owner;
+	CSharedPtr<class CGameObject> m_PlayerObject;
+	std::string	m_ClassTypeName;
+	std::string	m_PlayerObjectName;
+	CRenderManager* m_LayerName;
+
+protected:
+	void SetClassTypeName(const std::string& Name)
+	{
+		m_ClassTypeName = Name;
+	}
+
+public:
+	void SetPlayerObject(class CGameObject* Player);
+	void LoadComplete();
+
+public:
+	virtual bool Init();
+	virtual void Update(float DeltaTime);
+	virtual void PostUpdate(float DeltaTime);
+	virtual CSceneInfo* Clone();
+	virtual void Save(FILE* File);
+	virtual void Load(FILE* File);
+	virtual void SceneChangeComplete();
+};
+
